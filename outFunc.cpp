73d18ce4e56130e5ab5c667176cf1611ae1e56f1
whippingdot <whippingdot@outlook.com>
2025-06-02 22:07:09 +0530

    BALANCING OVER THANK GOD

--- outFunc.cpp
+++ outFunc.cpp
@@ -1,52 +1,55 @@
 #include "main.h"
 
-bool outPutRuns(int x, std::array<std::vector<char>, maxOvers> &array, int &runO, int &runT, int overN, int &ballN, int &outs, std::array<int, 2> &batsmen, std::array<int, 2> &index, bool &on, std::vector<std::vector<std::vector<int>>> &team, int inning, std::array<int, 2> &balls, std::vector<int> &partner, std::vector<std::vector<int>> &partners, bool &fHit, std::array<std::vector<std::array<int, 5>>, 2> &fall)
+bool outPutRuns(int x, std::array<std::vector<char>, maxOvers> &array, int &runO, int &runT, int overN, int &ballN, int &outs, std::array<int, 2> &batsmen, std::array<int, 2> &index, bool &on, std::vector<std::vector<std::vector<int>>> &team, int inning, std::array<int, 2> &balls, std::vector<int> &partner, std::vector<std::vector<int>> &partners, bool &fHit, std::array<std::vector<std::array<int, 5>>, 2> &fall, int &wickT, int &wickTCount, bool &added)
 {
   char number = ' ';
   int tRandom = std::rand() % 100 + 1;
   std::string nul = "";
+  added = false;
 
   if (fHit)
   {
-    std::cout << "FREE HIT: ";
+    //std::cout << "FREE HIT: ";
   }
 
   switch (x)
   {
   case 1:
-    std::cout << "Single (1)\n";
+    //std::cout << "Single (1)\n";
     number = '1';
     break;
   case 2:
-    std::cout << "2 runs\n";
+    //std::cout << "2 runs\n";
     number = '2';
     break;
   case 3:
-    std::cout << "3 runs\n";
+    //std::cout << "3 runs\n";
     number = '3';
     break;
   case 4:
-    std::cout << "Boundary (4)\n";
+    //std::cout << "Boundary (4)\n";
     number = '4';
     break;
   case 5:
-    std::cout << ".\n";
+    //std::cout << ".\n";
     break;
   case 6:
-    std::cout << "MAXIMUM (6)\n";
+    //std::cout << "MAXIMUM (6)\n";
     number = '6';
     break;
   case 7:
-    std::cout << "Wide (w)\n";
+    //std::cout << "Wide (w)\n";
     break;
   case 8:
-    std::cout << "OUT\n";
+    //std::cout << "OUT\n";
     break;
 
   default:
-    std::cout << "Well Something Broke!\n";
+    //std::cout << "Well Something Broke!\n";
     break;
   }
+
+
 
   if (x == 1 || x == 2 || x == 3 || x == 4 || x == 6)
   {
@@ -56,7 +59,7 @@ bool outPutRuns(int x, std::array<std::vector<char>, maxOvers> &array, int &runO
     {
       fHit = true;
       array[overN - 1].push_back('n');
-      std::cout << "\nNO BALL! Free hit next ball!\n";
+      //std::cout << "\nNO BALL! Free hit next ball!\n";
       runT++;
       runO++;
       partner[0]++;
@@ -84,18 +87,18 @@ bool outPutRuns(int x, std::array<std::vector<char>, maxOvers> &array, int &runO
 
     if (on)
     {
-      std::cout << "\n"
-                << names[inning - 1][index[0]] << "*: " << batsmen[0] << " in " << balls[0] << "\n";
-      std::cout << names[inning - 1][index[1]] << ": " << batsmen[1] << " in " << balls[1] << "\n\n";
+      //std::cout << "\n"
+                //<< names[inning - 1][index[0]] << "*: " << batsmen[0] << " in " << balls[0] << "\n";
+      //std::cout << names[inning - 1][index[1]] << ": " << batsmen[1] << " in " << balls[1] << "\n\n";
     }
     else
     {
-      std::cout << "\n"
-                << names[inning - 1][index[1]] << "*: " << batsmen[1] << " in " << balls[1] << "\n";
-      std::cout << names[inning - 1][index[0]] << ": " << batsmen[0] << " in " << balls[0] << "\n\n";
+      //std::cout << "\n"
+                //<< names[inning - 1][index[1]] << "*: " << batsmen[1] << " in " << balls[1] << "\n";
+      //std::cout << names[inning - 1][index[0]] << ": " << batsmen[0] << " in " << balls[0] << "\n\n";
     }
 
-    std::cout << "Current partnership: " << partner[0] << " in " << partner[1] << "\n";
+    //std::cout << "Current partnership: " << partner[0] << " in " << partner[1] << "\n";
 
     if (x == 1 || x == 3)
       on = !on;
@@ -108,7 +111,7 @@ bool outPutRuns(int x, std::array<std::vector<char>, maxOvers> &array, int &runO
     {
       fHit = true;
       array[overN - 1].push_back('n');
-      std::cout << "\nNO BALL! Free hit next ball!\n";
+      //std::cout << "\nNO BALL! Free hit next ball!\n";
       runT++;
       runO++;
       partner[0]++;
@@ -125,40 +128,40 @@ bool outPutRuns(int x, std::array<std::vector<char>, maxOvers> &array, int &runO
 
     if (on)
     {
-      std::cout << "\n"
-                << names[inning - 1][index[0]] << "*: " << batsmen[0] << " in " << balls[0] << "\n";
-      std::cout << names[inning - 1][index[1]] << ": " << batsmen[1] << " in " << balls[1] << "\n\n";
+      //std::cout << "\n"
+                //<< names[inning - 1][index[0]] << "*: " << batsmen[0] << " in " << balls[0] << "\n";
+      //std::cout << names[inning - 1][index[1]] << ": " << batsmen[1] << " in " << balls[1] << "\n\n";
     }
     else
     {
-      std::cout << "\n"
-                << names[inning - 1][index[1]] << "*: " << batsmen[1] << " in " << balls[1] << "\n";
-      std::cout << names[inning - 1][index[0]] << ": " << batsmen[0] << " in " << balls[0] << "\n\n";
+      //std::cout << "\n"
+                //<< names[inning - 1][index[1]] << "*: " << batsmen[1] << " in " << balls[1] << "\n";
+      //std::cout << names[inning - 1][index[0]] << ": " << batsmen[0] << " in " << balls[0] << "\n\n";
     }
-    std::cout << "Current partnership: " << partner[0] << " in " << partner[1] << "\n";
+    //std::cout << "Current partnership: " << partner[0] << " in " << partner[1] << "\n";
   }
   else if (x == 7)
   {
     array[overN - 1].push_back('w');
     if (fHit)
     {
-      std::cout << "\nNo ball continued next ball!\n";
+      //std::cout << "\nNo ball continued next ball!\n";
     }
     if (on)
     {
-      std::cout << "\n"
-                << names[inning - 1][index[0]] << "*: " << batsmen[0] << " in " << balls[0] << "\n";
-      std::cout << names[inning - 1][index[1]] << ": " << batsmen[1] << " in " << balls[1] << "\n\n";
+      //std::cout << "\n"
+                //<< names[inning - 1][index[0]] << "*: " << batsmen[0] << " in " << balls[0] << "\n";
+      //std::cout << names[inning - 1][index[1]] << ": " << batsmen[1] << " in " << balls[1] << "\n\n";
     }
     else
     {
-      std::cout << "\n"
-                << names[inning - 1][index[1]] << "*: " << batsmen[1] << " in " << balls[1] << "\n";
-      std::cout << names[inning - 1][index[0]] << ": " << batsmen[0] << " in "
-                << balls[0] << "\n\n";
+      //std::cout << "\n"
+                //<< names[inning - 1][index[1]] << "*: " << batsmen[1] << " in " << balls[1] << "\n";
+      //std::cout << names[inning - 1][index[0]] << ": " << batsmen[0] << " in "
+                //<< balls[0] << "\n\n";
     }
     partner[0]++;
-    std::cout << "Current partnership: " << partner[0] << " in " << partner[1] << "\n";
+    //std::cout << "Current partnership: " << partner[0] << " in " << partner[1] << "\n";
     runT++;
     runO++;
   }
@@ -168,7 +171,7 @@ bool outPutRuns(int x, std::array<std::vector<char>, maxOvers> &array, int &runO
     if ((tRandom == 18 || tRandom == 21) && !fHit)
     {
       array[overN - 1].push_back('n');
-      std::cout << "\nBUT ITS A NO BALL!\n";
+      //std::cout << "\nBUT ITS A NO BALL!\n";
       runT++;
       runO++;
       partner[0]++;
@@ -176,14 +179,14 @@ bool outPutRuns(int x, std::array<std::vector<char>, maxOvers> &array, int &runO
     else if (tRandom == 18 || tRandom == 21)
     {
       array[overN - 1].push_back('n');
-      std::cout << "\nBUT ITS A NO BALL AGAIN!\n";
+      //std::cout << "\nBUT ITS A NO BALL AGAIN!\n";
       runT++;
       runO++;
       partner[0]++;
     }
     else if (fHit)
     {
-      std::cout << "\nSince last ball was a no ball it doesn't count...\n";
+      //std::cout << "\nSince last ball was a no ball it doesn't count...\n";
       on ? balls[0]++ : balls[1]++;
       partner[1]++;
     }
@@ -199,9 +202,9 @@ bool outPutRuns(int x, std::array<std::vector<char>, maxOvers> &array, int &runO
       fOW[3] = overN - 1;
       fOW[4] = ballN + 1;
       fall[inning - 1].push_back(fOW);
-      std::cout << "\n"
-                << names[inning - 1][index[0]] << "*: " << batsmen[0] << " in " << balls[0] << " - OUT\n";
-      std::cout << names[inning - 1][index[1]] << ": " << batsmen[1] << " in " << balls[1] << "\n\n";
+      //std::cout << "\n"
+                //<< names[inning - 1][index[0]] << "*: " << batsmen[0] << " in " << balls[0] << " - OUT\n";
+      //std::cout << names[inning - 1][index[1]] << ": " << batsmen[1] << " in " << balls[1] << "\n\n";
       batsmen[0] = 0;
       balls[0] = 0;
       partner[2] = index[0];
@@ -227,9 +230,9 @@ bool outPutRuns(int x, std::array<std::vector<char>, maxOvers> &array, int &runO
       fOW[3] = overN - 1;
       fOW[4] = ballN + 1;
       fall[inning - 1].push_back(fOW);
-      std::cout << "\n"
-                << names[inning - 1][index[1]] << "*: " << batsmen[1] << " in " << balls[1] << " - OUT\n";
-      std::cout << names[inning - 1][index[0]] << ": " << batsmen[0] << " in " << balls[0] << "\n\n";
+      //std::cout << "\n"
+                //<< names[inning - 1][index[1]] << "*: " << batsmen[1] << " in " << balls[1] << " - OUT\n";
+      //std::cout << names[inning - 1][index[0]] << ": " << batsmen[0] << " in " << balls[0] << "\n\n";
       batsmen[1] = 0;
       balls[1] = 0;
       partner[2] = index[0];
@@ -248,22 +251,22 @@ bool outPutRuns(int x, std::array<std::vector<char>, maxOvers> &array, int &runO
     {
       if (on)
       {
-        std::cout << "\n"
-                  << names[inning - 1][index[0]] << "*: " << batsmen[0] << " in " << balls[0] << "\n";
-        std::cout << names[inning - 1][index[1]] << ": " << batsmen[1] << " in " << balls[1] << "\n\n";
+        //std::cout << "\n"
+                  //<< names[inning - 1][index[0]] << "*: " << batsmen[0] << " in " << balls[0] << "\n";
+        //std::cout << names[inning - 1][index[1]] << ": " << batsmen[1] << " in " << balls[1] << "\n\n";
       }
       else
       {
-        std::cout << "\n"
-                  << names[inning - 1][index[1]] << "*: " << batsmen[1] << " in " << balls[1] << "\n";
-        std::cout << names[inning - 1][index[0]] << ": " << batsmen[0] << " in " << balls[0] << "\n\n";
+        //std::cout << "\n"
+                  //<< names[inning - 1][index[1]] << "*: " << batsmen[1] << " in " << balls[1] << "\n";
+        //std::cout << names[inning - 1][index[0]] << ": " << batsmen[0] << " in " << balls[0] << "\n\n";
       }
-      std::cout << "Current partnership: " << partner[0] << " in " << partner[1] << "\n";
+      //std::cout << "Current partnership: " << partner[0] << " in " << partner[1] << "\n";
     }
     else
     {
       partner[1]++;
-      std::cout << "Broken partnership: " << partner[0] << " in " << partner[1] << "\n";
+      //std::cout << "Broken partnership: " << partner[0] << " in " << partner[1] << "\n";
       partners.push_back(partner);
       partner[0] = 0;
       partner[1] = 0;
@@ -278,10 +281,43 @@ bool outPutRuns(int x, std::array<std::vector<char>, maxOvers> &array, int &runO
       if (!fHit)
       {
         outs++;
+        wickT += 1;
+        added = true;
       }
       fHit = false;
       ballN++;
     }
+  }
+
+	int ball = (overN - 1) * 6 + ballN;
+  //std::cout << "WICKETS IN LAST 20: " << wickT << " + real ball no: " << ball << std::endl;
+  if (ball >= 26 && ball - wickTCount == 26) {
+    //std::cout << wickTCount << std::endl;
+		int remainder = wickTCount % 6;
+		//std::cout << remainder << std::endl;
+		int extras = 0;
+		for (int i = 0; i < array[wickTCount / 6].size(); i++) {
+			//std::cout << array[wickTCount / 6][i] << std::endl;
+			if (array[wickTCount / 6][i] == 'w') {
+				extras += 1;
+			}
+			else if (i + 1 != array[wickTCount / 6].size()) {
+				if (array[wickTCount / 6][i + 1] == 'n') {
+					extras += 2;
+				}
+			}
+			if (i - extras == remainder) {
+				break;
+			}
+		}
+		remainder += extras;
+
+
+		if (array[wickTCount / 6][remainder] == 'W') {
+			wickT--;
+		}
+
+    wickTCount++;
   }
 
   if (outs == 10)
